124e5cb94295847297974c54d41875ebb250ce92
Cedric van Putten <github@cedric.dev>
2023-04-21 16:32:09 +0200

    fix(template): use static entrypoint for ios

--- template/ios/HelloWorld/AppDelegate.mm
+++ template/ios/HelloWorld/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"HelloWorld", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"main", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
