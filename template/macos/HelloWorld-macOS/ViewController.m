2a56c46dd6140b365d7a4c860f6ea35a98d00bcc
Cedric van Putten <github@cedric.dev>
2023-04-21 16:52:36 +0200

    fix(template): use static entrypoint for macos viewcontroller

--- template/macos/HelloWorld-macOS/ViewController.m
+++ template/macos/HelloWorld-macOS/ViewController.m
@@ -9,7 +9,7 @@
   [super viewDidLoad];
 
   RCTBridge *bridge = [((AppDelegate *)[NSApp delegate])bridge];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"HelloWorld" initialProperties:nil];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"main" initialProperties:nil];
 
   NSView *view = [self view];
 
