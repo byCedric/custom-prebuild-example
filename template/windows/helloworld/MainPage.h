7ab6b62bb2dce9f2e55c939e7fa311d34f6ca3bf
Cedric van Putten <me@bycedric.com>
2023-04-22 13:31:04 +0200

    chore(template): add windows for react native 0.71

---
+++ template/windows/helloworld/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::helloworld::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::helloworld::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
